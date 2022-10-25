9ef734b834afc58c05d9b81c3de109f680d367eb
ajsaule <andrejsaule8@gmail.com>
2022-10-25 20:14:47 +1100

    fetch working, quotes showing on UI

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import styled from 'styled-components'
 import ButtonIcon from './svgs/ButtonIcon';
 import Divider from './svgs/Divider';
 
 const App = () => {
+  const [data, setData] = useState(null)
 
-  
+  useEffect(() => {
+    getAdvice()
+  }, [])
+
+  const getAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.slip);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <Wrapper>
       <Card>
-        <Heading>ADVICE #117</Heading>
-        <Body>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</Body>
+        <Heading>ADVICE #{data?.id}</Heading>
+        <Body>"{data?.advice}"</Body>
         <DividerWrapper>
           <Divider/>
         </DividerWrapper>
-        <Button activeOpacity={0.5}>
+        <Button activeOpacity={0.5} onPress={getAdvice}>
           <ButtonIcon/>
         </Button>
       </Card>
@@ -36,7 +52,7 @@ const Wrapper = styled.View`
 const Card = styled.View`
     justify-content: center;
     align-items: center;
-    height: 315px;
+    height: auto;
     width: 343px;
     padding: 24px;
     border-radius: 10px;
@@ -65,6 +81,9 @@ const Body = styled.Text`
     font-size: 24px;
     padding-top: 24px;
     line-height: 33px;
+    min-height: 132px;
+    text-align-vertical: center;
+    width: 295px;
     text-align: center;
     letter-spacing: -0.257143px;
     color: #CEE3E9;
@@ -75,11 +94,11 @@ const DividerWrapper = styled.View`
 `
 
 const Button = styled.TouchableOpacity`
-    position: relative;
+    position: fixed;
     display: flex;
     justify-content: center;
     align-items: center;
-    top: 25px;
+    top: 55px;
     background: #53FFAA;
     height: 64px;
     width: 64px;
