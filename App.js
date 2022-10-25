f0163a5d19546ab7ce4ac30821d34152000ace33
ajsaule <andrejsaule8@gmail.com>
2022-10-25 19:31:06 +1100

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Wrapper>
+      <Card>
+        <Heading>ADVICE #117</Heading>
+        <Body></Body>
+      </Card>
+    </Wrapper>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Wrapper = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: #202733;
+`
+
+const Card = styled.View`
+    justify-content: center;
+    align-items: center;
+    height: 315px;
+    width: 343px;
+    border-radius: 10px;
+    margin-bottom: 100px;
+    border-radius: 10px;
+    background: #313A48;
+    box-shadow: 30px 50px 80px rgba(0, 0, 0, 0.100202);
+`
+
+const Heading = styled.Text`
+    // font-family: 'Manrope';
+    // font-style: normal;
+    font-weight: 800;
+    font-size: 11px;
+    line-height: 15px;
+    text-align: center;
+    letter-spacing: 3.45714px;
+    color: #53FFAA;
+`
+
+const Body = styled.Text`
+    // font-family: 'Manrope';
+    // font-style: normal;
+    font-weight: 800;
+    font-size: 11px;
+    line-height: 15px;
+    text-align: center;
+    letter-spacing: 3.45714px;
+    color: #53FFAA;
+`
